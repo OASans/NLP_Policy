df369dba2fd185a3af2bc1c57dc2c6ca1180134e
OASans <171860015@smail.nju.edu.cn>
2021-10-06 23:01:25 +0800

    fix db

--- data_process/PolicyDB.py
+++ data_process/PolicyDB.py
@@ -1,10 +1,18 @@
+import os
+import re
+import glob
+import shutil
 import sqlite3
+import numpy as np
 import pandas as pd
 
 
 class PolicyDB:
     def __init__(self, db_name='policy.db'):
         self.conn = sqlite3.connect(db_name)
+
+    def close_db(self):
+        self.conn.close()
 
     def display_all_tables(self):
         c = self.conn.cursor()
@@ -54,6 +62,209 @@ class PolicyDB:
 
     def count_tagger(self):
         c = self.conn.cursor()
-        c.execute("""select count(name), * from tagger where status==1""")
+        c.execute("""select count(name) from tagger where status==1""")
+        print(c.fetchall())
+        c.execute("""select * from tagger where status==1""")
         print(c.fetchall())
 
+    def create_policy_table(self):
+        # if policy already exists
+        self.drop_table('policy')
+        self.create_table("""create table policy (
+                            uid varchar(20) primary key,
+                            origin_id varchar(20), 
+                            policy_name varchar(20),
+                            annotate_status int,
+                            tagger_name varchar(20),
+                            annotate_time varchar(20)
+                            )""")
+
+    def insert_policies(self):
+        cursor = self.conn.cursor()
+        folder_list = glob.glob('/Volumes/TOURO Mobil/policy标注/政策文件rawdata/*')
+        for folder in folder_list:
+            uid = folder.split('/')[-1]
+            origin_file_list = glob.glob('/Volumes/TOURO Mobil/policy标注/政策文件rawdata/{}/*'.format(uid))
+            origin_file_list = [file for file in origin_file_list if
+                                (file.split('.')[-1] != 'txt' and file.split('.')[-1] != 'xlsx')]
+            if len(origin_file_list) != 1:
+                print(uid)
+            else:
+                file_name = origin_file_list[0].split('/')[-1]
+                origin_id = re.split('[._]', file_name)[-2]
+                cursor.execute("INSERT INTO policy (uid,origin_id,policy_name,annotate_status,tagger_name, annotate_time) \
+                              VALUES ('{}', '{}', '{}', 0, '', '')".format(uid, origin_id, file_name))
+        self.conn.commit()
+
+    def create_annotated_sentence_table(self):
+        # if annotated_sentence already exists
+        self.drop_table('annotated_sentence')
+        self.create_table("""create table annotated_sentence (
+                            uid varchar(20),
+                            sid varchar(20) primary key,
+                            
+                            sentence text,
+                            sentence_type varchar(20)
+                            )""")
+
+    def create_entity_table(self):
+        # if entity already exists
+        self.drop_table('entity')
+        self.create_table("""create table entity (
+                            sid varchar(20),
+                            entity text,
+                            entity_type varchar(20)
+                            )""")
+
+    def create_entry_table(self):
+        # if entry already exists
+        self.drop_table('entry')
+        self.create_table("""create table entry (
+                            sid varchar(20),
+                            eid varchar(20),
+                            entry_type varchar(20),
+                            var text,
+                            relation varchar(20),
+                            field text,
+                            var_context text,
+                            field_context text,
+                            primary key (sid,eid)
+                            )""")
+
+    def create_entry_logic_table(self):
+        # if entry_logic already exists
+        self.drop_table('entry_logic')
+        self.create_table("""create table entry_logic (
+                            uid varchar(20),
+                            logic varchar(255),
+                            logic_name varchar(255)
+                            )""")
+
+    # 更新policy表状态
+    # info = (annotate_status, tagger_name, annotate_time, uid)
+    def update_policy(self, info):
+        sql = ''' UPDATE policy
+                  SET annotate_status = ? ,
+                      tagger_name = ? ,
+                      annotate_time = ?
+                  WHERE uid = ?'''
+        cur = self.conn.cursor()
+        cur.execute(sql, info)
+        self.conn.commit()
+
+    # 插入annotated_sentence表记录
+    # info = (uid, sid, sentence, sentence_type)
+    def insert_annotated_sentence(self, info):
+        sql = ''' INSERT INTO annotated_sentence(uid,sid,sentence,sentence_type)
+                  VALUES(?,?,?,?) '''
+        cur = self.conn.cursor()
+        cur.execute(sql, info)
+        self.conn.commit()
+
+    # 插入entity表记录
+    # info = (sid, entity, entity_type)
+    def insert_entity(self, info, sentence):
+        if info[2] == '发布时间' and info[1] not in sentence:
+            time = str(info[1])
+            time_match = re.search(r"(\d{4}\S\d{1,2}\S\d{1,2})", time)
+            if not time_match:
+                return
+            time_pattern = time_match.group(0)[:4] + "\S\d{1,2}\S\d{1,2}号*日*"
+            standard_time = re.search(time_pattern, sentence)
+            if not standard_time:
+                return
+            info = (info[0], standard_time.group(0), info[2])
+        sql = ''' INSERT INTO entity(sid,entity,entity_type)
+                  VALUES(?,?,?) '''
+        cur = self.conn.cursor()
+        cur.execute(sql, info)
+        self.conn.commit()
+
+    # 插入entry表记录
+    # info = (sid, eid, entry_type, var, relation, field, var_context, field_context)
+    def insert_entry(self, info):
+        sql = ''' INSERT INTO entry(sid, eid, entry_type, var, relation, field, var_context, field_context)
+                  VALUES(?,?,?,?,?,?,?,?) '''
+        cur = self.conn.cursor()
+        cur.execute(sql, info)
+        self.conn.commit()
+
+    # 插入entry_logic表记录
+    # info = (uid, logic,logic_name)
+    def insert_entry_logic(self, info):
+        sql = ''' INSERT INTO entry_logic(uid, logic, logic_name)
+                  VALUES(?,?,?) '''
+        cur = self.conn.cursor()
+        cur.execute(sql, info)
+        self.conn.commit()
+
+    # 读取一个新的文件，并将里面的信息分别插入到对应数据库表中，最后将这个读取过的文件移动到annotated_data文件夹
+    # 注意：此处直接添加数据，并未进行更多预处理（例如去除对于模型训练而言冗余的数据）
+    def read_new_file(self, file_path):
+        # file_path = './new_data/503-柏柯羽.xlsx'
+        path_items = re.split('[/.-]', file_path)
+        tagger_name = path_items[-2]
+        uid = str(int(path_items[-3]))
+        annotate_time = path_items[-4]
+
+        file_content = pd.read_excel(file_path, dtype=str).dropna(how='all').fillna('')
+        sentence_num = file_content.shape[0]
+
+        # 一些初步的数据预处理
+        # 将句子id列统一成int型
+        file_content['句子id'] = pd.to_numeric(file_content['句子id'], downcast='integer')
+
+        # 更新policy表中的文件状态
+        self.update_policy((1, tagger_name, annotate_time, uid))
+        # 对于每个句子
+        for i in range(sentence_num):
+            sid = uid + '_' + str(int(file_content.loc[i, '句子id']))
+            # 插入到annotated_sentence表
+            sentence = file_content.loc[i, '句子']
+            sentence_type = file_content.loc[i, '句子类型']
+            self.insert_annotated_sentence((uid, sid, sentence, sentence_type))
+            # 插入到entity表
+            for j in range(1, 6):
+                if file_content.loc[i, '实体' + str(j)] != '' and file_content.loc[i, '实体类别' + str(j)] != '':
+                    self.insert_entity((sid, file_content.loc[i, '实体' + str(j)], file_content.loc[i, '实体类别' + str(j)]),
+                                       sentence)
+            # 插入到entry表
+            for j in range(1, 16):
+                if file_content.loc[i, '准入条件id' + str(j)] != '':
+                    self.insert_entry((sid, file_content.loc[i, '准入条件id' + str(j)], file_content.loc[i, '准入条件类别' + str(j)],
+                                  file_content.loc[i, '变量' + str(j)], file_content.loc[i, '关系' + str(j)],
+                                  file_content.loc[i, '域' + str(j)],
+                                  file_content.loc[i, '变量{}的原文对应'.format(str(j))],
+                                  file_content.loc[i, '域{}的原文对应'.format(str(j))]))
+            # 插入到entry_logic表
+            if file_content.loc[i, '准入条件逻辑关系'] != '':
+                self.insert_entry_logic((uid, file_content.loc[i, '准入条件逻辑关系'], file_content.loc[i, '准入条件认定组别']))
+
+        # 将这个文件移动到annotated_data文件夹
+        shutil.move(file_path, './annotated_data/{}'.format(file_path.split('/')[-1]))
+
+        self.conn.commit()
+
+    def add_new_data(self, directory_path='./new_data'):
+        annotated_folder_list = glob.glob('{}/*-*'.format(directory_path))
+        for annotated_folder in annotated_folder_list:
+            annotated_files = glob.glob('{}/*.xlsx'.format(annotated_folder))
+            for file in annotated_files:
+                if '~' in file:
+                    continue
+                print(file)
+                self.read_new_file(file)
+
+
+if __name__ == '__main__':
+    db = PolicyDB()
+
+    db.delete_from_table('annotated_sentence')
+    db.delete_from_table('entity')
+    db.delete_from_table('entry')
+    db.delete_from_table('entry_logic')
+
+    db.add_new_data()
+
+
+    db.close_db()
